ab80be07579b9c2c7f2a60d804c62268905b1d6a
pixz <tinycactusking@gmail.com>
2025-04-02 18:22:09 -0400

    added pygame and main game loop

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while (True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+
+        pygame.display.flip()  # always call this last
 
 
 if __name__ == "__main__":
